7f3976ba493a060d8bd8c5e7c9fbfa98c4a1cbb0
oseiassoaresgithub <oseias.trabalhos@gmail.com>
2022-05-12 22:14:01 -0300

    jogo da velha

--- 1070/Program.cs
+++ 1070/Program.cs
@@ -7,15 +7,15 @@ namespace _1070
         static void Main(string[] args)
         {
             int X=0;
+            int Y=0;
 
             int.TryParse(Console.ReadLine(), out X);
 
-            for (int i = X; i <= X + 6; i++)
-            {
+            for (int i = X, y=1; y <=  6; i++) { 
                 if (i % 2 != 0)
                 {
                     Console.WriteLine(i);
-                    X++;
+                    y++;
                     
                 }
             }
--- matriz/Program.cs
+++ matriz/Program.cs
@@ -14,22 +14,153 @@ namespace matriz // jogo da velha
 
             InicializarTabuleiro();
 
+            int contador = 0;
+
             int quemjoga = 1;
             bool continuarjogando = true;
+            bool checarganhador, checarEmpate;
+            string posicaojogada;
 
 
             while (continuarjogando) // laço do jogo//
             {
+                
+
+
                 //imprimir quem joga
                 ImprimirTabuleiro();
                 //ler quem joga
+                posicaojogada = LerPosicaoJogador(quemjoga);
+
                 //registrar posição de jogada
+                RegistrarPosicaoJogador(quemjoga, posicaojogada);
                 //checar quem ganhou
+                checarganhador = ChecarGanhador(quemjoga);
+                
                 //checar empate
+                if (checarganhador == false)
+                {
+                    checarEmpate = ChecarEmpate();
+                    if(checarEmpate == true)
+                    {
+                        Console.WriteLine("Houve Empate!");
+                        continuarjogando=false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($" {quemjoga} é o vencedor!");
+                    continuarjogando=false;
+                }
+
+
                 //alterar quem joga
-                continuarjogando = false;
+                if (quemjoga == 1)
+                {
+                    quemjoga = 2;
+                }else if (quemjoga == 2)
+                {
+                    quemjoga = 1;
+                }
+
+                
+                //continuarjogando = false;
+                
+
 
             }
+            ImprimirTabuleiro();
+        }
+
+        static bool ChecarEmpate()
+        {
+            for (int i = 0; i < LINHAS; i++)
+            {
+                for (int j = 0; j < COLUNAS; j++)
+                {
+                    if (jogo[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static bool ChecarGanhador(int quemjoga)
+        {
+            if (jogo[0, 0] == quemjoga && jogo[0, 1] == quemjoga && jogo[0, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[1, 0] == quemjoga && jogo[1, 1] == quemjoga && jogo[1, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[2, 0] == quemjoga && jogo[2, 1] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == quemjoga && jogo[1, 0] == quemjoga && jogo[2, 0] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 1] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 1] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == quemjoga && jogo[1, 2] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 0] == quemjoga)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        static void RegistrarPosicaoJogador(int quemjoga, string posicaojogada)
+        {
+            int x, y;
+            var posicoes = posicaojogada.Split(' ');
+            int.TryParse(posicoes[0], out x);
+            int.TryParse(posicoes[1], out y);
+
+            jogo[x, y] = quemjoga;
+        }
+
+
+
+
+        static string LerPosicaoJogador(int quemjoga)
+        {
+            Console.WriteLine($"jogador {quemjoga} é a sua vez! escolha uma posicao ");
+            string linha;
+            int x, y;
+            /*
+             * 0 0 | 0 1 | 0 2
+             * 1 0 | 1 1 | 1 2
+             * 2 0 | 2 1 | 2 2
+             */
+            linha = Console.ReadLine();// 0 2
+            var posicoes = linha.Split(' ');
+            int.TryParse(posicoes[0], out x);
+            int.TryParse(posicoes[1], out y);
+
+
+            if (jogo[x, y] == 0)
+            {
+                // posicao nao jogada
+                return linha;
+            }
+            return "";
+
         }
 
         static void ImprimirTabuleiro()
@@ -41,7 +172,7 @@ namespace matriz // jogo da velha
                 {
                     if (jogo [i ,j] == 0)
                     {
-                        Console.Write(" ");
+                        Console.Write("   ");
                     }
                     else if (jogo [i ,j] == 1)
                     {
@@ -54,7 +185,7 @@ namespace matriz // jogo da velha
 
                     if (j == 0 || j == 1)
                     {
-                        Console.Write(" | ");
+                        Console.Write("|");
                     }
                         
 
@@ -63,7 +194,7 @@ namespace matriz // jogo da velha
                 Console.WriteLine();
                 if (i ==0 || i == 1)
                 {
-                    Console.WriteLine("--+---+--");
+                    Console.WriteLine("---+---+---");
                 }
             }
         }
